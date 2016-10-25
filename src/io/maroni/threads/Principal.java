7255442367e42cb1539fccf92eec3f1acfb61071
Vanderson Maroni <vandersongmaroni@gmail.com>
2016-10-24 23:04:06 -0200

    Commit inicial

---
+++ src/io/maroni/threads/Principal.java
@@ -0,0 +1,11 @@
+package io.maroni.threads;
+
+public class Principal {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		System.out.println("Isso é uma thread main");
+		
+		Thread.sleep(3000);
+	}
+}
