ff954d0bfc8540907634c622c8a820839e688ef0
Vanderson Maroni <vanderson.maroni@voxline.com.br>
2016-10-31 11:26:34 -0200

    Fazendo leitura de arquivos em paralelo

--- src/io/maroni/threads/Principal.java
+++ src/io/maroni/threads/Principal.java
@@ -3,9 +3,14 @@ package io.maroni.threads;
 public class Principal {
 
 	public static void main(String[] args) throws InterruptedException {
+		String nome = "da";
 		
-		System.out.println("Isso é uma thread main");
+		Thread threadAssinaturas1 = new Thread(new TarefaBuscaTextual("assinaturas1.txt",nome));
+		Thread threadAssinaturas2 = new Thread(new TarefaBuscaTextual("assinaturas2.txt",nome));
+		Thread threadAutores = new Thread(new TarefaBuscaTextual("autores.txt",nome));
 		
-		Thread.sleep(3000);
+		threadAssinaturas1.start();
+		threadAssinaturas2.start();
+		threadAutores.start();
 	}
 }
